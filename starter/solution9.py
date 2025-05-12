22e77d36f3f1a45202d5b4c39378d53a365686e6
SharifulW3 <shariful.w3@gmail.com>
2025-05-12 16:00:33 +0600

    add solution file on folders

--- starter/solution9.py
+++ starter/solution9.py
@@ -3,6 +3,11 @@
 
 my_list = [4, 8, 7, 4,3,6,2,1,9]
 
+print(my_list[:3])
+print(my_list[2:5])
+print(my_list[3:])
+print(my_list[:-2])
+
 for data in my_list:
     if data == 6:
         print("6 is available in the list!")
